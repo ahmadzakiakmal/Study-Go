595a63825977f581dca3de17919254dc114d7abb
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-08-25 09:32:37 +0700

    feat: add query based route

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 	
 	mux.HandleFunc("/", rootRouteHandler)
+	mux.HandleFunc("/product", productHandler)
 
 	log.Println("Starting server on port 5000...")
 	
@@ -22,4 +23,9 @@ func rootRouteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 }
 	w.Write([]byte("Hello world! I'm learning Golang Backend!"))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	w.Write([]byte("Product " + id))
 }
