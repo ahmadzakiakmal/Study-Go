fff109d04fde4ea5846c24fe7f84d93643049074
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-09-02 20:28:22 +0700

    feat: call method in HTML template

--- entity/product.go
+++ entity/product.go
@@ -6,3 +6,14 @@ type Product struct {
 	Price int
 	Stock int
 }
+
+func (p Product) StockStatus() string {
+	status := ""
+	if p.Stock < 10 {
+		status = "Stock is low"
+	}
+	if p.Stock < 3 {
+		status = "Stock is nearly out, order now!"
+	}
+	return status
+}
--- handler/handler.go
+++ handler/handler.go
@@ -22,11 +22,11 @@ func RootRouteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := map[string]interface{} {
-		"title": "Golang Web",
+	data := map[string]interface{}{
+		"title":   "Golang Web",
 		"content": "Index Page",
-	} 
-	
+	}
+
 	executeErr := tmpl.Execute(w, data)
 	if executeErr != nil {
 		log.Println(executeErr)
@@ -37,37 +37,72 @@ func RootRouteHandler(w http.ResponseWriter, r *http.Request) {
 
 func ProductHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
-	idNum, err := strconv.Atoi(id) 
+	idNum, err := strconv.Atoi(id)
 
 	// data := map[string]interface{} {
 	// 	"title" : "Product",
 	// 	"id": id,
-	// } 
+	// }
 
-	data := entity.Product{
-		Id: idNum,
-		Name: "Product " ,
-		Price: 5000,
-		Stock: 20,
+	data := []entity.Product{
+		{
+			Id:    0,
+			Name:  "Laptop",
+			Price: 25000,
+			Stock: 5,
+		},
+		{
+			Id:    1,
+			Name:  "Personal Computer",
+			Price: 45000,
+			Stock: 2,
+		},
+		{
+			Id:    2,
+			Name:  "Smartphone",
+			Price: 5000,
+			Stock: 15,
+		},
+		{
+			Id:    3,
+			Name:  "Headset",
+			Price: 1000,
+			Stock: 20,
+		},
 	}
+
+	var product entity.Product
+	found := false
 
 	if err != nil || idNum < 0 {
 		http.NotFound(w, r)
 		return
 	}
 
-	tmpl, err := template.ParseFiles(path.Join("views", "layout.html"), path.Join("views", "product.html"))
-	if err != nil {
-		log.Println(err, " Error parsing html files")
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	for _, p := range data {
+		if p.Id == idNum {
+			product = p
+			found = true
+			break
+		}
 	}
 
-	executeErr := tmpl.Execute(w, data)
-	if executeErr != nil {
-		log.Println(executeErr, " Error on data execute")
-		log.Println(data)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	if found {
+		tmpl, err := template.ParseFiles(path.Join("views", "layout.html"), path.Join("views", "product.html"))
+		if err != nil {
+			log.Println(err, " Error parsing html files")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		executeErr := tmpl.Execute(w, product)
+		if executeErr != nil {
+			log.Println(executeErr, " Error on data execute")
+			log.Println(data)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		http.NotFound(w, r)
 	}
 }
