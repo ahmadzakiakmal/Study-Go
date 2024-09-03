7f9b465a3d02a9c2d2327930d74a145b1cb4698e
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-09-03 20:24:58 +0700

    feat: prepare file server

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	
+
 	mux.HandleFunc("/", handler.RootRouteHandler)
 	mux.HandleFunc("/product", handler.ProductHandler)
 
+	fileServer := http.FileServer(http.Dir("assets"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on port 5000...")
-	
+
 	err := http.ListenAndServe(":5000", mux)
 	log.Fatal(err)
 }
